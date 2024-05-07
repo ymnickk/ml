952103fc58d70c2bb41bbad604bd3b31adf84f47
ymnickk <121603388+ymnickk@users.noreply.github.com>
2024-05-07 17:34:11 +0600

    Add files via upload

--- functions/init_df.py
+++ functions/init_df.py
@@ -2,7 +2,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/fire.csv")
-
+df[1] = [10]
+df[2] = [10]
+df[3] = [10]
+df[4] = [10]
+df[5] = [10]
+df[6] = [1]
+df[7] = [10]
 df = df.drop(["Unnamed: 0"], axis=1)
 X = df.drop('Fire Alarm_Yes', axis=1).values
 y = df['Fire Alarm_Yes']
