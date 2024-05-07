e0dac485fafb49b6d46d62043024c079a8e39528
ymnickk <121603388+ymnickk@users.noreply.github.com>
2024-05-07 15:56:53 +0600

    Add files via upload

--- functions/init_df.py
+++ functions/init_df.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("data/weather.csv")
+df = pd.read_csv("data/fire.csv")
 numeric_features = df.select_dtypes(include=["float64", "int64"]).columns
 df = df[numeric_features]
 
-df = df.drop(["Unnamed: 0", "index", "index.1"], axis=1)
-X = df.drop('RainTomorrow', axis=1).values
-y = df['RainTomorrow']
+df = df.drop(["Unnamed: 0"], axis=1)
+X = df.drop('Fire Alarm_Yes', axis=1).values
+y = df['Fire Alarm_Yes']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
